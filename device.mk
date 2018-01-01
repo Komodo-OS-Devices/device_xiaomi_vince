a4b5ba3193c1fe4f4a57369bc3f93b87adeb2686
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    vince: power: Switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Bug: 69270928
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- device.mk
+++ device.mk
@@ -377,14 +377,12 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor \
-    android.hardware.power-service-qti
+    android.hardware.power@1.2-service.vince-libperfmgr
 
+# Powerhint
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 
 # QMI
 PRODUCT_PACKAGES += \
